f2c3e0d2a8cd3bcf0939b21e53b59b4a56284114
TWTY\gray-chiu <gray-chiu@tsmt.com>
2025-02-01 16:40:21 +0800

    Refactor: Extract a query object

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,3 +1,4 @@
+import { GameQuery } from "../App";
 import useData from "./useData";
 import { Genre } from "./useGenres";
 
@@ -15,15 +16,16 @@ export interface Game {
   metacritic: number;
 }
 const useGames = (
-  selectedGenre: Genre | null, 
-  selectedPlatform: Platform | null
+  gameQuery: GameQuery
+  // selectedGenre: Genre | null, 
+  // selectedPlatform: Platform | null
 ) => {
   return useData<Game>('/games', {
     params: { 
-      genres: selectedGenre?.id, 
-      platforms: selectedPlatform?.id 
+      genres: gameQuery.genre?.id, 
+      platforms: gameQuery.platform?.id 
     }
-  }, [selectedGenre?.id, selectedPlatform?.id]);
+  }, [gameQuery]);
 }
 
 export default useGames;
