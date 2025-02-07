81784cbce4ae053f89b8a7ee3bcf886bb2e57ff0
TWTY\gray-chiu <gray-chiu@tsmt.com>
2025-02-07 23:26:56 +0800

    Fix: Failed to fetch platform PlayStation option.

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -21,7 +21,7 @@ const useGames = (
   return useData<Game>('/games', {
     params: { 
       genres: gameQuery.genre?.id, 
-      platforms: gameQuery.platform?.id,
+      parent_platforms: gameQuery.platform?.id,
       ordering: gameQuery.sortOrder,
       search: gameQuery.searchText
     }
