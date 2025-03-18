55c4f76845aaea02e945eec2351090c2d763df8a
TWTY\gray-chiu <gray-chiu@tsmt.com>
2025-03-18 23:34:56 +0800

    feat: Fetch game details

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -25,6 +25,12 @@ class ApiClient<T> {
       .then(res => res.data);
   }
 
+  get = (id: number | string) => {
+    return axiosInstance
+      .get<T>(this.endpoint + '/' + id)
+      .then(res => res.data);
+  }
+
 }
 
 export default ApiClient;
